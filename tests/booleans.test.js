3ae9db8931c5e8196297fd09b51428353762315a
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-06-22 20:25:33 +0200

    boleans test2 completed as required

--- tests/booleans.test.js
+++ tests/booleans.test.js
@@ -5,3 +5,8 @@ test('farm 1: list with 17 sheep', () => {
     const list = [ true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, true,  false, false, true , true,  true,  true,  true , false, false, true,  true ];
     expect(countSheep(list)).toBe('There are 17 sheep in total');
 });
+
+test('farm 2: list with no sheep', () => {
+    const list = [ false, false, false ];
+    expect(countSheep(list)).toBe('UPS!!! Wolfs eaten Sheeps');
+});
